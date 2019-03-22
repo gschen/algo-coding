f9db3f06d0d350ced78bf4cac700bdf685d9eb6e
chengongsuo <2547253080@qq.com>
2019-03-22 14:31:58 +0800

    sudoku finished.

--- 01-basic/src/edu/sctu/algo/basic/SudokuProblem.java
+++ 01-basic/src/edu/sctu/algo/basic/SudokuProblem.java
@@ -2,76 +2,117 @@ package edu.sctu.algo.basic;
 
 import edu.sctu.algo.utils.PrintUtils;
 
-
 //https://www.geeksforgeeks.org/sudoku-backtracking-7/
 public class SudokuProblem {
 
-    int[][] board = new int[][]
-            {
-                    {3, 0, 6, 5, 0, 8, 4, 0, 0},
-                    {5, 2, 0, 0, 0, 0, 0, 0, 0},
-                    {0, 8, 7, 0, 0, 0, 0, 3, 1},
-                    {0, 0, 3, 0, 1, 0, 0, 8, 0},
-                    {9, 0, 0, 8, 6, 3, 0, 0, 5},
-                    {0, 5, 0, 0, 9, 0, 6, 0, 0},
-                    {1, 3, 0, 0, 0, 0, 2, 5, 0},
-                    {0, 0, 0, 0, 0, 0, 0, 7, 4},
-                    {0, 0, 5, 2, 0, 6, 3, 0, 0}
-            };
+    int[][] board = new int[][]{
+            {3, 0, 6, 5, 0, 8, 4, 0, 0},
+            {5, 2, 0, 0, 0, 0, 0, 0, 0},
+            {0, 8, 7, 0, 0, 0, 0, 3, 1},
+            {0, 0, 3, 0, 1, 0, 0, 8, 0},
+            {9, 0, 0, 8, 6, 3, 0, 0, 5},
+            {0, 5, 0, 0, 9, 0, 6, 0, 0},
+            {1, 3, 0, 0, 0, 0, 2, 5, 0},
+            {0, 0, 0, 0, 0, 0, 0, 7, 4},
+            {0, 0, 5, 2, 0, 6, 3, 0, 0}
+    };
     int N = board.length;
 
-    private boolean solve(int x, int y) {
+    private boolean solve() {
 
-        if (x == N - 1 && y == N - 1) {
-            return true;
+        // 是否还有待处理的空位，即board[i][j] == 0的空位。
+        boolean waitToHandle = false;
+        int row = -1;
+        int col = -1;
+
+        // 找到值为0的空位，然后跳出循环。
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 0) {
+
+                    row = i;
+                    col = j;
+
+                    waitToHandle = true;
+                    break;
+                }
+            }
+
+            if (waitToHandle)
+                break;
         }
 
-        if (board[x][y] != 0 && isSafe(x, y)) {
+        if (!waitToHandle)
+            return true;
 
-            for (int i = 0; i < 9; i++) {
+        // 1-N 开始不停的试错
+        for (int num = 1; num <= N; num++) {
+            if (isSafe(row, col, num)) {
+                board[row][col] = num;
 
-                board[x][y] = i;
-
-                if (solve(x + 1, y)) {
+                if (solve())
                     return true;
-                }
 
-                if (solve(x, y + 1)) {
-                    return true;
-                }
-
+                board[row][col] = 0;
             }
         }
+
         return false;
     }
 
-    private boolean isSafe(int row, int col) {
+    private boolean isSafe(int row, int col, int num) {
 
+        // check row
         for (int i = 0; i < N; i++) {
-            if (board[i][col] == 1)
+            if (board[i][col] == num) {
+//                System.out.println("Row Error!");
                 return false;
+            }
         }
 
+        // check column
         for (int i = 0; i < N; i++) {
-            if (board[row][i] == 1)
+            if (board[row][i] == num) {
+//                System.out.println("Column Error!");
                 return false;
+            }
         }
 
+        // check palace
+        int palace = (int) Math.sqrt(N);
+        int palaceX = row / palace;
+        int palaceY = col / palace;
+
+        for (int i = palaceX * palace; i < (palaceX + 1) * palace; i++) {
+
+            for (int j = palaceY * palace; j < (palaceY + 1) * palace; j++) {
+
+                if (board[i][j] == num) {
+
+//                    System.out.println(String.format("Palace Error (%s, %s)", palaceX, palaceY));
+                    return false;
+                }
+            }
+        }
 
         return true;
     }
 
+
     public void run() {
 
-        solve(0, 0);
+        if (solve()) {
 
-        PrintUtils.printArray(board);
+            PrintUtils.printArray(board);
+
+        } else {
+            System.out.println("No Solution!");
+        }
 
     }
 
     public static void main(String[] args) {
 
         new SudokuProblem().run();
-
     }
 }
